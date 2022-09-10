7f1bfd4ab8678c165921577bdb41504c98e06a42
Haphiz <hafizosalau@gmail.com>
2022-09-10 14:57:28 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: list numbers less than 10
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
