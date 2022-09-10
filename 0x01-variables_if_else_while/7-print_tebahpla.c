0f6a7a8845748868b0463829ddc71738dcc89ab6
Haphiz <hafizosalau@gmail.com>
2022-09-10 16:43:50 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints letters in reverse
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	char s;
+
+	for (s = 'z'; s >= 'a'; s++)
+	{
+		putchar(s);
+	}
+	putchar('\n');
+	return (0);
+}
