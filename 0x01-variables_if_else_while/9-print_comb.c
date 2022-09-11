1d5caedd602fd784d4bf3c5b2cbaa29827895aa7
Haphiz <hafizosalau@gmail.com>
2022-09-11 15:36:22 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all possible combo of sigle-digits numbers
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
