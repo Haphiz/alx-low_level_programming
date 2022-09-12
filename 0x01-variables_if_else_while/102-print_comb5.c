b7e91f767ba566754e3a00909319fc724cfb1fe5
Haphiz <hafizosalau@gmail.com>
2022-09-12 16:18:23 -0700

    102-print_comb5.c5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints number 00 to 99 with conditions
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 98; x++)
+	{
+		for (y = 0; y <= 99; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar ((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 98 && y == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
