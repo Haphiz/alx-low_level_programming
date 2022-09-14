3730dfe75897cefeac47d045f414c287b50db792
Haphiz <hafizosalau@gmail.com>
2022-09-14 12:33:38 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of a number
+ * @n: integer type n
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
