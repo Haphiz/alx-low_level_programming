0faafb3ac5e6ba642f0c04d7c8ea013fb33e2f0d
Haphiz <hafizosalau@gmail.com>
2022-09-14 08:58:22 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10 - Print alphabe 10 times in lowercase using only _putchar
+ * Return: 0 Always Success
+ */
+void print_alphabet_10(void)
+{
+	int i, j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	}
+		_putchar('\n');
+}
