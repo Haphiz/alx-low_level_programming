39702852b3a22b84a9b4734af4da1c1eb52c35f4
Haphiz <hafizosalau@gmail.com>
2022-10-10 14:29:19 -0700

    Creating 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,13 +3,10 @@
 /**
  * main -prints the name of the file it was compiled from,
  * followed by a new line.
- * @argc: argument counter
- * @argv: argument value
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	printf("%s\n", __FILE__);
 	return (0);
 }
