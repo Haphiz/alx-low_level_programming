d0bcce0e3d4e1af65368498238083c13049c6d3e
Haphiz <hafizosalau@gmail.com>
2022-10-11 13:55:22 -0700

    Creating dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog basic information
+ * @name: first member of strucuture
+ * @age: second member
+ * @owner: third member
+ * Description: strucuture of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
